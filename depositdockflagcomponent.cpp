e9b6242bd2b14f03ba334f708ce7a7ade959d827
oaparfene <oaparfene@gmail.com>
2020-12-11 11:32:53 +0100

    added deposit dock

---
+++ depositdockflagcomponent.cpp
@@ -0,0 +1,6 @@
+#include "depositdockflagcomponent.h"
+
+DepositDockFlagComponent::DepositDockFlagComponent()
+{
+
+}
---
+++ depositdockflagcomponent.h
@@ -0,0 +1,11 @@
+#ifndef DEPOSITDOCKFLAGCOMPONENT_H
+#define DEPOSITDOCKFLAGCOMPONENT_H
+
+
+class DepositDockFlagComponent
+{
+public:
+    DepositDockFlagComponent();
+};
+
+#endif // DEPOSITDOCKFLAGCOMPONENT_H
